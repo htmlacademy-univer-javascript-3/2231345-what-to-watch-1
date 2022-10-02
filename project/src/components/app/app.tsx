07b57d71c8f0c3ae6b971edca316ae2968b61a53
Ilya Zhdanov <53978939+tewboat@users.noreply.github.com>
2022-10-02 17:38:09 +0500

    2.9. Начинаем программировать

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,8 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main-screen/main-screen';
+import {MainScreenProps} from '../../pages/main-screen/main-screen';
+
+function App(props: MainScreenProps): JSX.Element {
+  return <MainScreen name={props.name} genre={props.genre} releaseYear={props.releaseYear}/>;
 }
 
 export default App;
