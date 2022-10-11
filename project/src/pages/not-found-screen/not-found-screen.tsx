64891283bf0771bbd5983358b3c6927db8ccc07f
Ilya Zhdanov <53978939+tewboat@users.noreply.github.com>
2022-10-11 17:19:36 +0500

    fixup! feat: add routing

--- project/src/pages/not-found-screen/not-found-screen.tsx
+++ project/src/pages/not-found-screen/not-found-screen.tsx
@@ -1,11 +1,12 @@
 import {Link} from 'react-router-dom';
+import {AppRoute} from '../../consts';
 
 function NotFoundScreen(){
   return(
-    <>
-      <strong>404 Not Found</strong>
-      <Link to='/'>To Main Page</Link>
-    </>
+    <div style={{width: '50%', margin: '0 auto', textAlign: 'center'}}>
+      <p><strong>404 Not Found</strong></p>
+      <Link to={AppRoute.Main}>To Main Page</Link>
+    </div>
   );
 }
 
