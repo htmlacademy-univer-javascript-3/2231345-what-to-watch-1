8fd7fc546e30794a90739ea84bde535dbaf45688
Ilya Zhdanov <zdravulin@gmail.com>
2022-10-23 16:08:25 +0500

    Больше подробностей (часть 1)

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -49,8 +49,7 @@ function MainScreen({headerFilm, films}: MainScreenProps) {
               </p>
 
               <div className="film-card__buttons">
-                <button className="btn btn--play film-card__button" type="button"
-                        onClick={() => navigate(`/player/${headerFilm.id}`)}>
+                <button className="btn btn--play film-card__button" type="button" onClick={() => navigate(`/player/${headerFilm.id}`)}>
                   <svg viewBox="0 0 19 19" width="19" height="19">
                     <use xlinkHref="#play-s"></use>
                   </svg>
